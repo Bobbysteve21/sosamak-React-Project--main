dfab481a68701ea68624ebffef88431eef139844
Bobbysteve21 <103030264+Bobbysteve21@users.noreply.github.com>
2022-10-14 01:24:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sosamak-React-Project--main
+ 
---
+++ sosamak-React-Project--main/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import Body from "./files /Body"
+import Body2 from "./files /Body2"
+import Navbar from "./files /Navbar"
+
+
+function App() {
+
+  return (
+    <div className="App">
+      {/* <i className="fas fa-bars"></i> */}
+     <Navbar />
+     <Body />
+     <Body2 />
+    </div>
+  )
+}
+
+export default App
